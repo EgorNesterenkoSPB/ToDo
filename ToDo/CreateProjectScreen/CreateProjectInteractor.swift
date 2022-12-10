5c11d13cd732af585dac7cef95f152720f60190a
Egor Nesterenko <nesterenkoegorbussines@gmail.com>
2022-12-11 00:13:57 +0300

    create logic to protect creating project w/ name Incoming

--- ToDo/CreateProjectScreen/CreateProjectInteractor.swift
+++ ToDo/CreateProjectScreen/CreateProjectInteractor.swift
@@ -3,6 +3,10 @@ import Foundation
 final class CreateProjectInteractor:PresenterToInteractorCreateProjectProtocol {
     func onCreateProject(name: String, hexColor: String, isFavorite: Bool) {
         do {
+            guard name != Resources.incomingProjectName else { // protect from create project with name "Incoming" bc we init project with this name at first when user open main screen
+                self.presenter?.failureCreateProject(errorText: "You cant create project with this name")
+                return
+            }
             var projects = try DataManager.shared.projects()
             let project = DataManager.shared.project(name: name, hexColor: hexColor, isFavorite: isFavorite)
             projects.append(project)
